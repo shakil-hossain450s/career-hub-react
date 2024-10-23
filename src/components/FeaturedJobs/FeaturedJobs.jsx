162b584a80c6ab45f22cb8e2197f15fa28505f7a
shakil <shakil.hossain450s@gmail.com>
2024-10-24 00:55:56 +0600

    show all jobs button functionality added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,8 @@ import Button from '../Button/Button';
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [dataLength, setDataLength] = useState(4);
+
     useState(() => {
         fetch('./jobs.json')
             .then(res => res.json())
@@ -16,11 +18,13 @@ const FeaturedJobs = () => {
             <SectionHeading heading={"Featured Jobs"}></SectionHeading>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
             </div>
-            <div className='grid place-items-center mt-10'>
-                <Button btnName={"See All Jobs"}></Button>
+            <div className={`grid place-items-center mt-10 ${dataLength === jobs.length && "hidden"}`}>
+                <div onClick={() => { setDataLength(jobs.length) }}>
+                    <Button btnName={"See All Jobs"}></Button>
+                </div>
             </div>
         </div>
     );
